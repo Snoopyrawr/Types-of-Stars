5f56fbe04f5155ef358bdd793c8e8278f36efe51
Redjyll Brint <iredjyllbrint@gmail.com>
2024-07-31 22:04:28 +0800

    Update App.tsx

--- star-types/src/App.tsx
+++ star-types/src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Post from "./components/Posts"
 
-const App = () => {
+const App = () : JSX.Element => {
 
   return (
     <>
